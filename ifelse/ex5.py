3e58a2adfe68cc6abfec5b6bd87c57ab39486d6e
MAPH1N <sardarxdd@gmail.com>
2023-02-09 15:49:39 +0600

    lab2

--- ifelse/ex5.py
+++
@@ -1,7 +0,0 @@
-a=1
-b=1
-c=1
-d=1
-
-if a == b and c == d:
-  print("Hello")
--- lists/ex7.py
+++
@@ -1,4 +0,0 @@
-fruits=["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(fruits[2:5])
-
-#cherry, orrange, kiwi
